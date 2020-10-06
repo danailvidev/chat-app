6c0d18b60bae58caf4ca6b8ad26a783079e6364d
Danail Videv <danailvidev@gmail.com>
2020-10-06 22:12:49 +0300

    add dashboard module

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { Observable } from 'rxjs';
+
+import { AuthService } from 'src/app/core/services/auth.service';
+import { MessageService } from 'src/app/core/services/message.service';
 
 @Component({
   selector: 'fe-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [style({ opacity: 0 }), animate('0.5s ease-out', style({ opacity: 1 }))]),
+      transition(':leave', [style({ opacity: 1 }), animate('0.5s ease-in', style({ opacity: 0 }))])
+    ])
+  ]
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  public users$: Observable<any> = this.db.collection('users').valueChanges();
+  public showUserModal: boolean = false;
+  public users: any;
+  public selectedUser;
 
-  ngOnInit(): void {}
+  @ViewChild('therapist') therapistInput: ElementRef;
+  @ViewChild('currentUser') userInput: ElementRef;
+
+  constructor(
+    private messageSvc: MessageService,
+    private cd: ChangeDetectorRef,
+    private authSvc: AuthService,
+    private db: AngularFirestore
+  ) {}
+
+  ngOnInit(): void {
+    this.users$.subscribe((users) => {
+      this.users = users;
+    });
+  }
+
+  onUserSelected(user) {
+    this.selectedUser = null;
+    setTimeout(() => {
+      this.selectedUser = user;
+      this.cd.detectChanges();
+    }, 500);
+  }
+
+  onModalCancel() {
+    this.showUserModal = false;
+  }
+
+  onNewUserCreate(user) {
+    this.authSvc.createUser({ name: user.name }).subscribe((x) => {
+      this.onUserSelected({ name: user.name });
+    });
+  }
+
+  onTherapistTextSend() {
+    this.therapistInput.nativeElement.value = '';
+    console.log(this.therapistInput.nativeElement.value);
+  }
+
+  oncurrentUserTextSend() {
+    console.log(this.userInput.nativeElement.value, this.selectedUser);
+    const message = this.userInput.nativeElement.value;
+    this.messageSvc.createMessage(this.selectedUser, message);
+    this.userInput.nativeElement.value = '';
+  }
 }
--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard.routing';
+import { UserListModule } from './user-list/user-list.module';
+import { CreateModalModule } from './create-modal/create-modal.module';
 
 @NgModule({
-  imports: [CommonModule, DashboardRoutingModule],
+  imports: [CommonModule, FormsModule, DashboardRoutingModule, UserListModule, CreateModalModule],
   declarations: [DashboardComponent]
 })
 export class DashboardModule {}
